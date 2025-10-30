822bcc47df605d8b9910ffe532a8bc812019a04c
henrique.faria <henriquedantas007@gmail.com>
2025-10-30 10:27:40 -0300

    classe produto

--- src/main/java/br/com/API/projeto/model/Produto.java
+++ src/main/java/br/com/API/projeto/model/Produto.java
@@ -9,19 +9,37 @@ import org.hibernate.validator.constraints.UniqueElements;
 import org.hibernate.validator.constraints.br.CPF;
 
 
+@Data
+
 @Entity
 @Table(name = "produto")
 public class Produto {
 
-private int id;
 
-private String nome;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //DIZ Q VAI GERAR O ID AUTOMATICO
+    @Column(name = "id")
+    private int id;
 
-private float preco_custo;
 
-private float preco_venda;
+    @NotBlank(message = "O nome é obrigatório!")
+    @Column(name = "nome", length = 255, nullable = false) // tamanho e not null
+    private String nome;
 
-private int qntd_estoque;
+
+    @NotBlank(message = "O preço-custo é obrigatório!")
+    @Column(name = "preco_custo", nullable = false) // tamanho e not null
+    private float preco_custo;
+
+
+    @NotBlank(message = "O preço de venda é obrigatório!")
+    @Column(name = "preco_venda", nullable = false) // tamanho e not null
+    private float preco_venda;
+
+
+    @NotBlank(message = "A quantidade de estoque é obrigatória!")
+    @Column(name = "qntd_estoque", nullable = false) // tamanho e not null
+    private int qntd_estoque;
 
 
 
